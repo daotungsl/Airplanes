8cd4b77e3505f3baa0550ffb14f3cf60dea14183
Pham Tien Bac <tienbac250496@gmail.com>
2019-01-16 02:10:22 +0700

    Tạo register va login. cung voi security

--- Airplanes/Startup.cs
+++ Airplanes/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Airplanes.Models;
 
 namespace Airplanes
 {
@@ -33,6 +35,10 @@ namespace Airplanes
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<AirplanesContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("AirplanesContext")));
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
