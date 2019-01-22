210a4af42936f48481763d69a150b871aa129ae9
tungdm <tungdm@gitlab.nal.vn>
2019-01-22 11:08:00 +0700

    Done 8 plane

--- Airplanes/Migrations/20190121163257_SeedDbAirport.cs
+++ Airplanes/Migrations/20190121163257_SeedDbAirport.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
 
 namespace Airplanes.Migrations
 {
@@ -6,7 +7,237 @@ namespace Airplanes.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            // Airport Viet Nam
+            string[] AirportNameVN = { "Côn Đảo", "Phù Cát", "Cà Mau", "Cần Thơ", "Buôn Ma Thuột",
+                                    "Đà Nẵng", "Điện Biên Phủ", "Pleiku", "Cát Bi", "Nội Bài",
+                                    "Tân Sơn Nhất", "Cam Ranh", "Rạch Giá", "Phú Quốc", "Liên Khương",
+                                    "Vinh", "Tuy Hòa", "Đồng Hới", "Chu Lai", "Phú Bài", "Thọ Xuân", "Vân Đồn" };
+          
+            for (int num = 0; num < AirportNameVN.Length; num++)
+            {
+                migrationBuilder.InsertData(
+                 table: "DbAirport",
+                 columns: new[] { "AirportName", "DbCityId","CreatedAt", "UpdatedAt" },
+                 values: new object[] { AirportNameVN[num], num+1, DateTime.Now, DateTime.Now }
+                 );
+            };
 
+            //City China
+            string[] AirportNameChina = { "Bắc Kinh", "Hồng Kiều", "Bạch Vân", "Tiêu Sơn", "Song Lưu", "Hồng Kông", "Macau" };
+            
+
+            for (int num = 0; num < AirportNameChina.Length; num++)
+            {
+                migrationBuilder.InsertData(
+                 table: "DbAirport",
+                 columns: new[] { "AirportName", "DbCityId", "CreatedAt", "UpdatedAt" },
+                 values: new object[] { AirportNameChina[num], num + 23, DateTime.Now, DateTime.Now }
+                 );
+            };
+
+
+            //City Dai loan
+            string[] AirportNameDailoan = { "Đào Viên", "Cao Hùng" };
+
+            for (int num = 0; num < AirportNameDailoan.Length; num++)
+            {
+                migrationBuilder.InsertData(
+                 table: "DbAirport",
+                 columns: new[] { "AirportName", "DbCityId", "CreatedAt", "UpdatedAt" },
+                 values: new object[] { AirportNameDailoan[num], num + 30, DateTime.Now, DateTime.Now }
+                 );
+            };
+
+            //City Japan
+            string[] AirportNameJapan = { "Fukuoka", "Chubu Centrair", "Kansai", "Tokyo", "Osaka" };
+
+            for (int num = 0; num < AirportNameJapan.Length; num++)
+            {
+                migrationBuilder.InsertData(
+                 table: "DbAirport",
+                 columns: new[] { "AirportName", "DbCityId", "CreatedAt", "UpdatedAt" },
+                 values: new object[] { AirportNameJapan[num], num + 32, DateTime.Now, DateTime.Now }
+                 );
+            };
+
+            //City korea
+            string[] AirportNameKorea = { "Gimhae", "Incheon" };
+
+            for (int num = 0; num < AirportNameKorea.Length; num++)
+            {
+                migrationBuilder.InsertData(
+                 table: "DbAirport",
+                 columns: new[] { "AirportName", "DbCityId", "CreatedAt", "UpdatedAt" },
+                 values: new object[] { AirportNameKorea[num], num + 37, DateTime.Now, DateTime.Now }
+                 );
+            };
+
+            //City Campuchia
+            string[] AirportNameCampuchia = { "Phnom Penh", "Siem Reap" };
+
+            for (int num = 0; num < AirportNameCampuchia.Length; num++)
+            {
+                migrationBuilder.InsertData(
+                 table: "DbAirport",
+                 columns: new[] { "AirportName", "DbCityId", "CreatedAt", "UpdatedAt" },
+                 values: new object[] { AirportNameCampuchia[num], num + 39, DateTime.Now, DateTime.Now }
+                 );
+            };
+
+            //City laos
+            string[] AirportNameLaos = { "Wattay", "Luang Phrabang" };
+
+            for (int num = 0; num < AirportNameLaos.Length; num++)
+            {
+                migrationBuilder.InsertData(
+                  table: "DbAirport",
+                  columns: new[] { "AirportName", "DbCityId", "CreatedAt", "UpdatedAt" },
+                  values: new object[] { AirportNameLaos[num], num + 41, DateTime.Now, DateTime.Now }
+                  );
+            };
+
+            //City Indo
+            string[] AirportNameIndonesia = { "Soekarno-Hatta", "Ngurah Rai", "Kuala Namu" };
+
+            for (int num = 0; num < AirportNameIndonesia.Length; num++)
+            {
+                migrationBuilder.InsertData(
+                 table: "DbAirport",
+                 columns: new[] { "AirportName", "DbCityId", "CreatedAt", "UpdatedAt" },
+                 values: new object[] { AirportNameIndonesia[num], num + 43, DateTime.Now, DateTime.Now }
+                 );
+            };
+
+            //City malaysia
+            migrationBuilder.InsertData(
+                 table: "DbAirport",
+                 columns: new[] { "AirportName", "DbCityId", "CreatedAt", "UpdatedAt" },
+                 values: new object[] { "Kuala Lumpur", 46, DateTime.Now, DateTime.Now }
+                 );
+
+            //City Singapore
+            migrationBuilder.InsertData(
+                 table: "DbAirport",
+                 columns: new[] { "AirportName", "DbCityId", "CreatedAt", "UpdatedAt" },
+                 values: new object[] { "Changi", 47, DateTime.Now, DateTime.Now }
+                 );
+
+            //Cyti France
+            string[] AirportNameFrance = { "Charles De Gaulle", "Marseille Provence", "Montpellier", "Nice" };
+
+            for (int num = 0; num < AirportNameFrance.Length; num++)
+            {
+                migrationBuilder.InsertData(
+                 table: "DbAirport",
+                 columns: new[] { "AirportName", "DbCityId", "CreatedAt", "UpdatedAt" },
+                 values: new object[] { AirportNameFrance[num], num + 48, DateTime.Now, DateTime.Now }
+                 );
+            };
+
+            //City Germany
+            migrationBuilder.InsertData(
+                 table: "DbAirport",
+                 columns: new[] { "AirportName", "DbCityId", "CreatedAt", "UpdatedAt" },
+                 values: new object[] { "Frankfurt", 52, DateTime.Now, DateTime.Now }
+                 );
+
+            //City England
+            migrationBuilder.InsertData(
+                 table: "DbAirport",
+                 columns: new[] { "AirportName", "DbCityId", "CreatedAt", "UpdatedAt" },
+                 values: new object[] { "London", 53, DateTime.Now, DateTime.Now }
+                 );
+
+            //City Australia 
+            string[] AirportNameAustralia = { "Melbourne", "Sydney", "Darwin" };
+
+            for (int num = 0; num < AirportNameAustralia.Length; num++)
+            {
+                migrationBuilder.InsertData(
+                 table: "DbAirport",
+                 columns: new[] { "AirportName", "DbCityId", "CreatedAt", "UpdatedAt" },
+                 values: new object[] { AirportNameAustralia[num], 54, DateTime.Now, DateTime.Now }
+                 );
+            };
+
+            //City New Zealand
+            string[] AirportNameZealand = { "Auckland", "Christchurch", "Wellington" };
+
+            for (int num = 0; num < AirportNameZealand.Length; num++)
+            {
+                migrationBuilder.InsertData(
+                 table: "DbAirport",
+                 columns: new[] { "AirportName", "DbCityId", "CreatedAt", "UpdatedAt" },
+                 values: new object[] { AirportNameZealand[num], 57, DateTime.Now, DateTime.Now }
+                 );
+            };
+
+            //City Thailand
+            string[] AirportNameThailand = { "Survanabhumi", "Phuket" };
+
+            for (int num = 0; num < AirportNameThailand.Length; num++)
+            {
+                migrationBuilder.InsertData(
+                 table: "DbAirport",
+                 columns: new[] { "AirportName", "DbCityId", "CreatedAt", "UpdatedAt" },
+                 values: new object[] { AirportNameThailand[num], 60, DateTime.Now, DateTime.Now }
+                 );
+            };
+
+            //City Nigeria
+            migrationBuilder.InsertData(
+                 table: "DbAirport",
+                 columns: new[] { "AirportName", "DbCityId", "CreatedAt", "UpdatedAt" },
+                 values: new object[] { "Angeles", 62, DateTime.Now, DateTime.Now }
+                 );
+
+            //City Russia
+            migrationBuilder.InsertData(
+                 table: "DbAirport",
+                 columns: new[] { "AirportName", "DbCityId", "CreatedAt", "UpdatedAt" },
+                 values: new object[] { "Moscow", 63, DateTime.Now, DateTime.Now }
+                 );
+
+            //City South Africa
+            string[] AirportNameAfrica = { "Cape Town", "OR Tambo" };
+
+            for (int num = 0; num < AirportNameAfrica.Length; num++)
+            {
+                migrationBuilder.InsertData(
+                 table: "DbAirport",
+                 columns: new[] { "AirportName", "DbCityId", "CreatedAt", "UpdatedAt" },
+                 values: new object[] { AirportNameAfrica[num], 64, DateTime.Now, DateTime.Now }
+                 );
+            };
+
+            //City United States
+            string[] AirportNameUS = {  "Hartsfield", "Logan","Dallas-Forth Worth","Denver","Honolulu",
+                                    "Los Angeles","Miami","Minneapolis","John F.Kennedy","Philadephia",
+                                    "Portland","San Francisco","Seattle – Tacoma","Lambert" };
+
+
+            for (int num = 0; num < AirportNameUS.Length; num++)
+            {
+                migrationBuilder.InsertData(
+                 table: "DbAirport",
+                 columns: new[] { "AirportName", "DbCityId", "CreatedAt", "UpdatedAt" },
+                 values: new object[] { AirportNameUS[num], num + 66, DateTime.Now, DateTime.Now }
+                 );
+            };
+
+            //City Myanmar
+            migrationBuilder.InsertData(
+                 table: "DbAirport",
+                 columns: new[] { "AirportName", "DbCityId", "CreatedAt", "UpdatedAt" },
+                 values: new object[] { "Yangon", 80, DateTime.Now, DateTime.Now }
+                 );
+
+            //City Czech Republic
+            migrationBuilder.InsertData(
+                 table: "DbAirport",
+                 columns: new[] { "AirportName", "DbCityId", "CreatedAt", "UpdatedAt" },
+                 values: new object[] { "Havel Prava", 81, DateTime.Now, DateTime.Now }
+                 );
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
---
+++ Airplanes/Migrations/20190122025714_SeedDbAplane.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+namespace Airplanes.Migrations
+{
+    public partial class SeedDbAplane : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            string[] PlaneName = { "Airbus A330-300", "Airbus A350-900XWB", "Boeing 787-9 Dreamliner", "Boeing 737", "Boeing 737 MAX",
+                                    "Boeing 777","Airbus A321","Airbus A380", };
+            string[] MadeIn = { "Airbus S.A.S","Airbus S.A.S","Boeing BCA",
+                                "Boeing BCA", "Boeing BCA",
+                                "Boeing BCA","Airbus S.A.S","Airbus S.A.S" };
+            string[] Image = { "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Usairways_a330-300_n278ay_arp.jpg/1280px-Usairways_a330-300_n278ay_arp.jpg",
+                                "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/A350_First_Flight_-_Low_pass_02.jpg/1024px-A350_First_Flight_-_Low_pass_02.jpg",
+                                "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/All_Nippon_Airways_Boeing_787-8_Dreamliner_JA801A_OKJ.jpg/1024px-All_Nippon_Airways_Boeing_787-8_Dreamliner_JA801A_OKJ.jpg",
+                                "https://upload.wikimedia.org/wikipedia/commons/2/26/AirBerlin_B737-800_D-ABBF_MUC_2008-08-13.jpg",
+                                "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/WS_YYC_737_MAX_1.jpg/1024px-WS_YYC_737_MAX_1.jpg",
+                                "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Boeing_777-219ER_ZK-OKA_Air_New_Zealand_(7031891827).jpg/1024px-Boeing_777-219ER_ZK-OKA_Air_New_Zealand_(7031891827).jpg",
+                                "https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/D-AIAF_(19552328044).jpg/1024px-D-AIAF_(19552328044).jpg",
+                                "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Emirates_Airbus_A380-861_A6-EER_MUC_2015_02.jpg/1920px-Emirates_Airbus_A380-861_A6-EER_MUC_2015_02.jpg" };
+            string[] UrlTemplate = {"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Usairways_a330-300_n278ay_arp.jpg/1280px-Usairways_a330-300_n278ay_arp.jpg",
+                                "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/A350_First_Flight_-_Low_pass_02.jpg/1024px-A350_First_Flight_-_Low_pass_02.jpg",
+                                "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/All_Nippon_Airways_Boeing_787-8_Dreamliner_JA801A_OKJ.jpg/1024px-All_Nippon_Airways_Boeing_787-8_Dreamliner_JA801A_OKJ.jpg",
+                                "https://upload.wikimedia.org/wikipedia/commons/2/26/AirBerlin_B737-800_D-ABBF_MUC_2008-08-13.jpg",
+                                "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/WS_YYC_737_MAX_1.jpg/1024px-WS_YYC_737_MAX_1.jpg",
+                                "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Boeing_777-219ER_ZK-OKA_Air_New_Zealand_(7031891827).jpg/1024px-Boeing_777-219ER_ZK-OKA_Air_New_Zealand_(7031891827).jpg",
+                                "https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/D-AIAF_(19552328044).jpg/1024px-D-AIAF_(19552328044).jpg",
+                                "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Emirates_Airbus_A380-861_A6-EER_MUC_2015_02.jpg/1920px-Emirates_Airbus_A380-861_A6-EER_MUC_2015_02.jpg" };
+
+            for (int num = 0; num < PlaneName.Length; num++)
+            {
+                migrationBuilder.InsertData(
+                 table: "DbPlane",
+                 columns: new[] { "PlaneName", "MadeIn", "Image", "UrlTemplate", "CreatedAt", "UpdatedAt" },
+                 values: new object[] { PlaneName[num], MadeIn[num], Image[num], UrlTemplate[num], DateTime.Now, DateTime.Now }
+                 );
+            };
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
